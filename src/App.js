172b53a0e10c79feacffcd96d2db8faea83c934d
ybmin1 <yebin.min1@gmail.com>
2024-07-22 16:56:47 +0900

    w3 assignment-customize react background

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { WiDayHaze } from "react-icons/wi";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <WiDayHaze className="icon-wiDayHaze" />
+        <p>Week3 Assignment</p>
         <a
           className="App-link"
           href="https://reactjs.org"
