ba6125c6b9c3bb69d9fd456feee373e49205c3a0
ybmin1 <yebin.min1@gmail.com>
2024-07-24 21:27:22 +0900

    revised static data location

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import "./App.css";
 import { WiDayHaze } from "react-icons/wi";
+import "./App.css";
+
+const title = "Week3 Assignment";
+const url = "https://reactjs.org";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <WiDayHaze className="icon-wiDayHaze" />
-        <p>Week3 Assignment</p>
+        <p>{title}</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href={url}
           target="_blank"
           rel="noopener noreferrer"
         >
